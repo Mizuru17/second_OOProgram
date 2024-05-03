4919f5dd165c2f38bda4ee8fa329ac728ae4da25
Shapoval <nastasyia259@gmail.com>
2024-05-03 21:26:24 +0300

    Лaбораторна робота №7. ООП. Створення об'єктів класу (мова Java)

--- src/Car.java
+++ src/Car.java
@@ -2,12 +2,10 @@ public class Car {
     private String brand;
     private String model;
 
-
     public Car(String brand, String model) {
         this.brand = brand;
         this.model = model;
     }
-
 
     public String getBrand() {
         return brand;
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ public class Main {
         Person person1 = new Person("Іван", 25);
         Person person2 = new Person("Марія", 30);
 
-
         Car car1 = new Car("Toyota", "Camry");
         Car car2 = new Car("Honda", "Civic");
-
 
         System.out.println("Персона 1: " + person1.getName() + ", " + person1.getAge() + " років");
         System.out.println("Персона 2: " + person2.getName() + ", " + person2.getAge() + " років");
